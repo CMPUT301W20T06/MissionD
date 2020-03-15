09f595ec91aa6c6f60b527e4dec2d08a4abd716b
Isaac <Isaaczhang11@gmail.com>
2020-03-14 23:13:15 -0600

    create register pages

---
+++ app/src/main/java/com/example/missiond/Register.java
@@ -0,0 +1,17 @@
+package com.example.missiond;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    public Button confirm;
+    public text viw;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
