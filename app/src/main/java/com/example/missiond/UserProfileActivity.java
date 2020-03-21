dfdf3898016c6e1b58c7512e7d313e92fa5b5e5f
changxin <changxin@ualberta.ca>
2020-03-21 09:16:39 -0600

    changes on profile

--- app/src/main/java/com/example/missiond/UserProfileActivity.java
+++ app/src/main/java/com/example/missiond/UserProfileActivity.java
@@ -3,31 +3,42 @@ package com.example.missiond;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserProfileActivity extends AppCompatActivity {
     private Button close;
+    private DataBaseHelper db;
+    private TextView nameBig;
+    private EditText mName,mPhone,mEmail;
+    private String name,phone,email;
+    private Driver currentDriver;
+    private Rider currentRider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
 
-//        DataBaseHelper DB = DataBaseHelper.getInstance();
-//        final Rider rider = DB.getRider("Isaac");
-//        String rider_name = "Isaac";
-//        String rider_phone = rider.getPhoneNumber();
-//        String rider_email = rider.getEmailAddress();
-//        TextView phone = findViewById(R.id.editPhone_profile);
-//        TextView email = findViewById(R.id.editEmail_profile);
-//        TextView name = findViewById(R.id.editName_profile);
-//        name.setText(rider_name);
-//        phone.setText(rider_phone);
-//        email.setText(rider_email);
-
+        db = DataBaseHelper.getInstance();
+        nameBig = findViewById(R.id.user_name);
+        mName = findViewById(R.id.editName_profile);
+        mPhone = findViewById(R.id.editPhone_profile);
+        mEmail = findViewById(R.id.editEmail_profile);
         close = findViewById(R.id.profileBack);
+        currentDriver = db.getDriver("Yifei");
+        name = currentDriver.getUserName();
+        phone = currentDriver.getPhoneNumber();
+        email = currentDriver.getEmailAddress();
+        nameBig.setText(name);
+        mName.setText(name);
+        mPhone.setText(phone);
+        mEmail.setText(email);
+
+
+
         close.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
