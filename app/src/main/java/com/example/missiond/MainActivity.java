59ff985d86987f710cddf677fc483f874db38ac0
WeiyiWu <wwu3@ualberta.ca>
2020-04-02 22:09:13 -0600

    fixed UI

--- app/src/main/java/com/example/missiond/MainActivity.java
+++ app/src/main/java/com/example/missiond/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 //    private String pickupLoc,DestLoc,tempLoc;
     private Button button_rider;
     private Button button_driver;
-    private Button button_register;
+    private TextView button_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
